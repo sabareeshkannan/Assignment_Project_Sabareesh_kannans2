ea62c4d1ba62031d136ff12c0aafaaba3801f28c
sabareeshkannan <122717719+sabareeshkannan@users.noreply.github.com>
2025-09-17 12:45:08 -0500

    Create Superuser and register models in admin.py

--- mealprepped/admin.py
+++ mealprepped/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Ingredient, Recipe, RecipeIngredient, MealPlan, MealPlanEntry
 
 # Register your models here.
+admin.site.register(Ingredient)
+admin.site.register(Recipe)
+admin.site.register(RecipeIngredient)
+admin.site.register(MealPlan)
+admin.site.register(MealPlanEntry)
