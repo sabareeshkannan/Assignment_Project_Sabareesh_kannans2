49b7cb3ebb404d526f0c3de52128354cca5311d3
sabareeshkannan <122717719+sabareeshkannan@users.noreply.github.com>
2025-10-09 18:45:19 -0500

    Update urls.py

--- mealprepped/urls.py
+++ mealprepped/urls.py
@@ -1,13 +1,14 @@
 # mealprepped/urls.py
 from django.urls import path
-from .views import ingredient_list, recipe_list, week_entries, IngredientDetailView, MealPlanDetailView, MealPlanListView, RecipeDetailView
+from .views import ingredient_list, week_entries, IngredientDetailView, MealPlanDetailView, \
+    MealPlanListView, RecipeDetailView, RecipeListView
 
 app_name = "mealprepped"
 
 urlpatterns = [
-    path("", recipe_list, name="home"),
+    path("", RecipeListView.as_view(), name="home"),
     path("ingredients-list/", ingredient_list, name="ingredients_list"),
-    path("recipes-list/", recipe_list, name="recipes_list"),
+    path("recipes-list/", RecipeListView.as_view(), name="recipes_list"),
     path("plan/week/", week_entries, name="week_entries"),
     path("mealplan/<int:primary_key>/", MealPlanDetailView.as_view(), name="mealplan_detail"),
     path("mealplans-list/", MealPlanListView.as_view(), name="mealplans_list"),
